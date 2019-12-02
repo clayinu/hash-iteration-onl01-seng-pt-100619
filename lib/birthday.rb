e322cc8671d9305cc3bd213554b82cbaae7b91d7
Hoko Le <hokole527@gmail.com>
2019-12-02 00:25:20 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,9 +6,15 @@
 
 def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
+    
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    
+  end
+  
+  birthday_kids.each do |kids_name, age|
     if age <= 12
-      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-    end
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
   end
 end
 
